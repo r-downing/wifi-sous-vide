59f1b2e334e1b145795740172a0cd2f7979727d1
r-downing <26515643+r-downing@users.noreply.github.com>
2017-11-10 13:32:29 -0500

    Updated to use SPIFFSReadServer lib

--- wifi-sous-vide.ino
+++ wifi-sous-vide.ino
@@ -18,12 +18,12 @@
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
 #include <FS.h>
+#include <SPIFFSReadServer.h>
 
 #define RELAY_PIN D7
 #define PULSEWIDTH 5000
 
 #define DEVICE_NAME "Sous Vide"
-
 
 //temperature sensor libraries and variables
 #include <OneWire.h>
@@ -37,8 +37,6 @@ unsigned long timeAtTemp;
 bool relayControl, powerOn;
 AutoPIDRelay myPID(&temperature, &setTemp, &relayControl, 5000, .12, .0003, 0);
 
-const char *metaRefreshStr = "<head><meta http-equiv=\"refresh\" content=\"1; url=/\" /></head><body><a href=\"/\">redirecting...</a></body>";
-
 unsigned long lastTempUpdate;
 void updateTemperature() {
   if ((millis() - lastTempUpdate) > TEMP_READ_DELAY) {
@@ -48,69 +46,10 @@ void updateTemperature() {
   }
 } //void updateTemperature
 
-ESP8266WebServer server(80);
+SPIFFSReadServer server(80);
 int scannedNetworks, scanssid;
 DNSServer dnsServer;
 PersWiFiManager persWM(server, dnsServer);
-
-
-//code from fsbrowser example, consolidated.
-bool handleFileRead(String path) {
-  DEBUG_PRINT("handlefileread" + path);
-  if (path.endsWith("/")) path += "index.htm";
-  String contentType;
-  if (path.endsWith(".htm") || path.endsWith(".html")) contentType = "text/html";
-  else if (path.endsWith(".css")) contentType = "text/css";
-  else if (path.endsWith(".js")) contentType = "application/javascript";
-  else if (path.endsWith(".png")) contentType = "image/png";
-  else if (path.endsWith(".gif")) contentType = "image/gif";
-  else if (path.endsWith(".jpg")) contentType = "image/jpeg";
-  else if (path.endsWith(".ico")) contentType = "image/x-icon";
-  else if (path.endsWith(".xml")) contentType = "text/xml";
-  else if (path.endsWith(".pdf")) contentType = "application/x-pdf";
-  else if (path.endsWith(".zip")) contentType = "application/x-zip";
-  else if (path.endsWith(".gz")) contentType = "application/x-gzip";
-  else if (path.endsWith(".json")) contentType = "application/json";
-  else contentType = "text/plain";
-
-  //split filepath and extension
-  String prefix = path, ext = "";
-  int lastPeriod = path.lastIndexOf('.');
-  if (lastPeriod >= 0) {
-    prefix = path.substring(0, lastPeriod);
-    ext = path.substring(lastPeriod);
-  }
-
-  //look for smaller versions of file
-  //minified file, good (myscript.min.js)
-  if (SPIFFS.exists(prefix + ".min" + ext)) path = prefix + ".min" + ext;
-  //gzipped file, better (myscript.js.gz)
-  if (SPIFFS.exists(prefix + ext + ".gz")) path = prefix + ext + ".gz";
-  //min and gzipped file, best (myscript.min.js.gz)
-  if (SPIFFS.exists(prefix + ".min" + ext + ".gz")) path = prefix + ".min" + ext + ".gz";
-
-  if (SPIFFS.exists(path)) {
-    DEBUG_PRINT("sending file " + path);
-    File file = SPIFFS.open(path, "r");
-    if (server.hasArg("download"))
-      server.sendHeader("Content-Disposition", " attachment;");
-    if (server.uri().indexOf("nocache") < 0)
-      server.sendHeader("Cache-Control", " max-age=172800");
-
-    //optional alt arg (encoded url), server sends redirect to file on the web
-    if (WiFi.status() == WL_CONNECTED && server.hasArg("alt")) {
-      server.sendHeader("Location", server.arg("alt"), true);
-      server.send ( 302, "text/plain", "");
-    } else {
-      //server sends file
-      size_t sent = server.streamFile(file, contentType);
-    }
-    file.close();
-    return true;
-  } //if SPIFFS.exists
-  return false;
-} //bool handleFileRead
-
 
 void setup() {
   DEBUG_START; //for terminal debugging
@@ -129,14 +68,6 @@ void setup() {
 
   persWM.setApCredentials(DEVICE_NAME);
   persWM.begin();
-
-  //serve files from SPIFFS
-  server.onNotFound([]() {
-    if (!handleFileRead(server.uri())) {
-      server.sendHeader("Cache-Control", " max-age=172800");
-      server.send(302, "text/html", metaRefreshStr);
-    }
-  }); //server.onNotFound
 
   //handles commands from webpage, sends live data in JSON format
   server.on("/io", []() {
